c09e5a7e95c23538b2bee37721d3a4090013718f
Colin Inskeep <colinjinskeep@gmail.com>
2026-04-02 09:18:07 -0700

    adding vercel.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,16 +3,5 @@ import tailwindcss from '@tailwindcss/vite'
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-        replenishment: './src/pages/Replenishment.jsx',
-        purchase_orders: './src/pages/PurchaseOrders.jsx',
-        // ...
-        // List all files you want in your build
-      }
-    }
-  }
+  plugins: [tailwindcss(), react()]
 });
