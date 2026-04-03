d83c7039a3cd7afe964bbc458746c2ce72983e54
Colin Inskeep <colinjinskeep@gmail.com>
2026-04-03 00:55:10 -0700

    define ENV

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,11 @@ import tailwindcss from '@tailwindcss/vite'
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react()]
+  plugins: [tailwindcss(), react()],
+  define: {
+
+    "process.env.API_KEY": JSON.stringify(process.env.API_KEY)
+
+  }
 });
+
