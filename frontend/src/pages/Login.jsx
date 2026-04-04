410ba939ac1c230e4110151bb5d6c672dc44fcef
Colin Inskeep <colinjinskeep@gmail.com>
2026-04-03 17:10:55 -0700

    correct env variables

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = process.env.API_URL;
 
 export default function Login() {
   const { login } = useAuth();
