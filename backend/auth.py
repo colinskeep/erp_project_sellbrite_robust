d52c07653c66ef54e4f27112fbd39837a6afdd2e
Colin Inskeep <colinjinskeep@gmail.com>
2026-04-03 22:16:47 -0700

    update auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -15,5 +15,7 @@ def verify_password(plain, hashed):
     return pwd_context.verify(plain, hashed) 
     
 def create_access_token(data: dict): 
-    to_encode = data.copy() expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES) to_encode.update({"exp": expire}) 
+    to_encode = data.copy() 
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES) 
+    to_encode.update({"exp": expire}) 
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
